f1336d55d3af7420d9734382d4aedf923ea191bc
Devshah-10 <72973324+Devshah-10@users.noreply.github.com>
2022-01-04 22:21:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+
+var player_1 = document.getElementById("Text_1");
+var player_2 = document.getElementById("Text_2");
+
+localStorage.setItem("player 1 name is ", player_1);
+localStorage.setItem("player 2 name is ", player_2);
+
+function Nav() {
+
+    window.location = "SecondPg.html";
+    
+    }
